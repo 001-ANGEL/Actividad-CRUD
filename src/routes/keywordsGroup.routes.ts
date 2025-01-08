dea10901d68adbce2cd0c57d6442a90b2c03d639
Angel Lopez <angellopezixcayau@gmail.com>
2025-01-08 13:51:19 -0600

    Created routes for Written Content and Keyword Group

---
+++ src/routes/keywordsGroup.routes.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+
+
+const router = Router();
+
+router.post("/api/keyword-groups", );
+
+router.get("/api/keyword-groups", );
+
+router.get("/api/keyword-groups/:id", );
+
+router.put("/api/keyword-groups/:id", );
+
+router.delete("/api/keyword-groups/:id", );
+
+export default router;
---
+++ src/routes/writtenContent.routes.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+
+
+const router = Router();
+
+router.post("/api/written-content", );
+
+router.get("/api/written-content", );
+
+router.get("/api/written-content/:id", );
+
+router.put("/api/written-content/:id", );
+
+router.delete("/api/written-content/:id", );
+
+export default router;
